879830a673c5346cce198fddf454439f43168d7c
Fernando158 <Fernando.salas2102@gmail.com>
2019-05-02 09:51:31 -0400

    Funcionando Static

--- analysticar-django/settings.py
+++ analysticar-django/settings.py
@@ -126,9 +126,21 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Directorio de templates y de statics.
+
+STATICFILES_DIRS= [os.path.join(BASE_DIR,'static')]
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 LOGGING = {
      'version': 1,
